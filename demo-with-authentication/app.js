3fc2116f0e422e7ba33942eb99cc6f11cfb7081d
Mark McCahill <mark.mccahill@duke.edu>
2017-05-01 16:59:39 -0400

    add passport bearker token authentication
    Signed-off-by: Mark McCahill <mark.mccahill@duke.edu>

---
+++ demo-with-authentication/app.js
@@ -0,0 +1,111 @@
+var express = require('express');
+var app = express();
+var expressWs = require('express-ws')(app);
+var os = require('os');
+var pty = require('node-pty');
+
+var passport = require('passport');
+var Strategy = require('passport-http-bearer').Strategy;
+
+// Configure the Bearer strategy for use by Passport.
+//
+// The Bearer strategy requires a `verify` function which receives the
+// credentials (`token`) contained in the request.  The function must invoke
+// `cb` with a user object, which will be set at `req.user` in route handlers
+// after authentication.
+passport.use(new Strategy(
+  function(token, cb) {
+      console.log('checking bearer token: ' + token);
+      // the environmental variable NICETOKEN will be passed into the docker container
+      // so this is what we check the bearer token against
+      var a_nice_token = process.env.NICETOKEN;
+      console.log('reading token from env: ', a_nice_token);
+      console.log('comparison is: ', ( token == a_nice_token ));
+      if ( token == a_nice_token ) {
+        // the generic user in the docker container is 'student'
+        return cb(null, 'student');
+      } else {  
+        return cb(null, false); 
+      }
+  }));
+
+var terminals = {},
+    logs = {};
+
+app.use('/build', express.static(__dirname + '/../build'));
+
+app.get('/', 
+  passport.authenticate('bearer', { session: false }),
+  function(req, res){
+  res.sendFile(__dirname + '/index.html');
+});
+
+app.get('/style.css', function(req, res){
+  res.sendFile(__dirname + '/style.css');
+});
+
+app.get('/main.js', function(req, res){
+  res.sendFile(__dirname + '/main.js');
+});
+
+app.post('/terminals', function (req, res) {
+  var cols = parseInt(req.query.cols),
+      rows = parseInt(req.query.rows),
+      term = pty.spawn(process.platform === 'win32' ? 'cmd.exe' : 'bash', [], {
+        name: 'xterm-color',
+        cols: cols || 80,
+        rows: rows || 30,
+        cwd: process.env.PWD,
+        env: process.env
+      });
+
+  console.log('Created terminal with PID: ' + term.pid);
+  terminals[term.pid] = term;
+  logs[term.pid] = '';
+  term.on('data', function(data) {
+    logs[term.pid] += data;
+  });
+  res.send(term.pid.toString());
+  res.end();
+});
+
+app.post('/terminals/:pid/size', function (req, res) {
+  var pid = parseInt(req.params.pid),
+      cols = parseInt(req.query.cols),
+      rows = parseInt(req.query.rows),
+      term = terminals[pid];
+
+  term.resize(cols, rows);
+  console.log('Resized terminal ' + pid + ' to ' + cols + ' cols and ' + rows + ' rows.');
+  res.end();
+});
+
+app.ws('/terminals/:pid', function (ws, req) {
+  var term = terminals[parseInt(req.params.pid)];
+  console.log('Connected to terminal ' + term.pid);
+  ws.send(logs[term.pid]);
+
+  term.on('data', function(data) {
+    try {
+      ws.send(data);
+    } catch (ex) {
+      // The WebSocket is not open, ignore
+    }
+  });
+  ws.on('message', function(msg) {
+    term.write(msg);
+  });
+  ws.on('close', function () {
+    term.kill();
+    console.log('Closed terminal ' + term.pid);
+    // Clean things up
+    delete terminals[term.pid];
+    delete logs[term.pid];
+  });
+});
+
+var port = process.env.PORT || 3000,
+    host = os.platform() === 'win32' ? '127.0.0.1' : '0.0.0.0';
+
+console.log('App listening to http://' + host + ':' + port);
+app.listen(port, host);
