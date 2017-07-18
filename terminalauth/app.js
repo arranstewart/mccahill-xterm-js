f811ba1a8edd203f7ab6eec40a0a3f695a33b63d
Mark McCahill <mark.mccahill@duke.edu>
2017-07-18 12:56:18 -0400

    round 2
    Signed-off-by: Mark McCahill <mark.mccahill@duke.edu>

--- terminalauth/app.js
+++ terminalauth/app.js
@@ -149,8 +149,8 @@ app.ws('/terminals/:pid', function (ws, req) {
 });
 
 app.get('/download-archive', function(req, res){
-//    passport.authenticate('local', { failureRedirect: '/loginfail' }),
-    function(req, res){	
+    //passport.authenticate('local', { failureRedirect: '/loginfail' });
+ //   function(req, res){	
 	    var archive = archiver('zip');	
 
 	    archive.on('error', function(err) {
@@ -173,7 +173,7 @@ app.get('/download-archive', function(req, res){
 
 	    archive.directory('/home/student', false);
 	    archive.finalize();
-//    }
+ //   }
 });
 
 var port = process.env.PORT || 3000,
