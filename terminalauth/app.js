bf168ac424730876484b343d1864934585371bbe
Mark McCahill <mark.mccahill@duke.edu>
2017-07-18 17:38:04 -0400

    once again
    Signed-off-by: Mark McCahill <mark.mccahill@duke.edu>

--- terminalauth/app.js
+++ terminalauth/app.js
@@ -148,7 +148,7 @@ app.ws('/terminals/:pid', function (ws, req) {
   });
 });
 
-app.get('/test-archive', 
+app.get('/download-archive', 
   passport.authenticate('local', { failureRedirect: '/loginfail' }),
   function(req, res){
     var archive = archiver('zip');	
@@ -176,30 +176,6 @@ app.get('/test-archive',
 });
 
 
-app.get('/download-archive', function(req, res){	
-	    var archive = archiver('zip');	
-
-	    archive.on('error', function(err) {
-	      res.status(500).send({error: err.message});
-	    });
-
-	    //on stream closed we can end the request
-	    archive.on('end', function() {
-	      console.log('Archive wrote %d bytes', archive.pointer());
-	    });
-
-	    //set the archive name
-	    res.attachment('archive-download.zip');
-
-	    //this is the streaming magic
-	    archive.pipe(res);
-	
-		// pipe archive data to the file
-		//archive.pipe(output);
-
-	    archive.directory('/home/student', false);
-	    archive.finalize();
-});
 
 var port = process.env.PORT || 3000,
     host = os.platform() === 'win32' ? '127.0.0.1' : '0.0.0.0';
